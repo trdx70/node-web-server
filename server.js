360ed25cfc86a49c287ced70fd15e543162e6396
Sonny Trinidad <strinidad70@gmail.com>
2017-07-14 17:10:01 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n',(err)=>{
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+})
+
+
+app.get('/',(req, res)=>{
+  // // res.send('Hello Express');
+  // res.send({
+  //   name: 'Sonny',
+  //   like: ['biking','food']
+  // })
+  res.render('home.hbs',{
+    pageTitle: 'Home Page Express',
+    welcomeMessage: 'Hello from Express'
+  })
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+  });
+})
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage: 'Bad data'
+  })
+})
+
+
+app.listen(port,()=>{
+  console.log(`Server is up and running on port 3000 ${port}`);
+});
